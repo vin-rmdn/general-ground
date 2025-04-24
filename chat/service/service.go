a553a3c9f17a31df9d2abe67af1424ca58fdc8ad
Alvin Januar <aljan_r@outlook.co.id>
2025-04-24 16:16:48 +0700

    feat: explicit logging on get service
    Signed-off-by: Alvin Januar <aljan_r@outlook.co.id>

--- chat/service/service.go
+++ chat/service/service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"fmt"
+	"log/slog"
 	"time"
 
 	"github.com/vin-rmdn/general-ground/chat"
@@ -23,15 +24,31 @@ func New(repository repository) service {
 	}
 }
 
+// Get retrieves chat messages for a specific recipient.
+// In my example, Get will be added with metrics directly in the code
 func (s service) Get(ctx context.Context, to string) ([]chat.Chat, error) {
+	from, _ := ctx.Value("user").(string)
+	slog.Debug("Get chat messages", slog.String("to", to), slog.String("from", from))
+
 	chats, err := s.repository.Get(ctx, to)
 	if err != nil {
+		slog.Error(
+			"Failed to get chat messages",
+			slog.String("to", to),
+			slog.String("from", from),
+			slog.String("error", err.Error()),
+		)
+
 		return nil, fmt.Errorf("failed to get chats: %v", err)
 	}
+
+	slog.Debug("Successfully retrieved chat messages", slog.String("to", to), slog.String("from", from), slog.Any("chats", chats))
 
 	return chats, nil
 }
 
+// Chat sends a message to a specific recipient and saves it to the repository.
+// In my example, Chat metrics will be added as a middleware
 func (s service) Chat(ctx context.Context, to, message string) error {
 	if err := s.repository.Save(ctx, to, message, time.Now()); err != nil {
 		return fmt.Errorf("failed to save chat: %v", err)
